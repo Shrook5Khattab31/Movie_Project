25d81b7ae0e6063f4b3b016088a1162751dd54ca
shrook <shrookabdelmonem55@gmail.com>
2025-11-05 08:39:04 +0200

    + Project Initialization

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MovieApp());
+}
+
+class MovieApp extends StatelessWidget {
+  const MovieApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
