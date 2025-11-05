eaeb4b74c027d1348de5140fb36c44b25e4db993
nada <nadamoafia66@gmail.com>
2025-11-05 16:24:45 +0200

    feat: add all app image assets (icons, backgrounds)

---
+++ lib/core/constants/appAssets.dart
@@ -0,0 +1,27 @@
+class AppImages{
+  static const String onboarding1 = 'assets/images/onboarding1.png';
+  static const String onboarding2 = 'assets/images/onboarding2.png';
+  static const String onboarding3 = 'assets/images/onboarding3.png';
+  static const String onboarding4 = 'assets/images/onboarding4.png';
+  static const String onboarding5 = 'assets/images/onboarding5.png';
+  static const String onboardingEnd = 'assets/images/onboarding_end.png';
+  static const String avatar1 = 'assets/images/avatar1.png';
+  static const String avatar2 = 'assets/images/avatar1.png';
+  static const String avatar3 = 'assets/images/avatar1.png';
+  static const String egypt = 'assets/images/egypt.png';
+  static const String usa = 'assets/images/usa.png';
+  static const String forgotPasswordImage = 'assets/images/forgot_password.png';
+  static const String emptyList = 'assets/images/empty_popcorn.png';
+  static const String appLogo = 'assets/images/app_logo.png';
+  static const String emailIcon = 'assets/images/email_icon.png';
+  static const String passwordIcon = 'assets/images/password_icon.png';
+  static const String userIcon = 'assets/images/user_icon.png';
+  static const String googleIcon = 'assets/images/google_icon.png';
+  static const String homeIcon = 'assets/images/home_icon.png';
+  static const String searchIcon = 'assets/images/search_icon.png';
+  static const String browseIcon = 'assets/images/browse_icon.png';
+  static const String profileIcon = 'assets/images/profile_icon.png';
+  static const String likeIcon = 'assets/icons/like_icon.png';
+  static const String bookmarkIcon = 'assets/icons/bookmark_icon.png';
+  static const String starIcon = 'assets/icons/star_icon.png';
+}
