25f11f7d685357b0dbe988c6ea7aa7d6ce5f356c
shrook <shrookabdelmonem55@gmail.com>
2025-11-07 16:46:47 +0200

    +[fix]: minor changes in onBoarding

--- lib/Features/onBoarding/onboarding_one.dart
+++ lib/Features/onBoarding/onboarding_one.dart
@@ -28,10 +28,14 @@ class OnboardingOne extends StatelessWidget {
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-              width: 430,
-              height: screenHeight * 0.33,
+              width: screenWidth,
+              height: screenHeight * 0.37,
               decoration: const BoxDecoration(
-                color: AppColors.primaryColor,
+                gradient: LinearGradient(
+                  colors: [AppColors.transparentColor, Color(0xE6121312), AppColors.primaryColor],
+                  begin: AlignmentGeometry.topCenter,
+                  end: AlignmentGeometry.bottomCenter,
+                ),
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(40),
                   topRight: Radius.circular(40),
@@ -41,38 +45,41 @@ class OnboardingOne extends StatelessWidget {
                 horizontal: screenWidth * 0.06,
                 vertical: screenHeight * 0.02,
               ),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      lang.find_next_favorite_movie,
-                      style: AppStyles.bold24White,
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: screenHeight * 0.015),
-                    Text(
-                      lang.access_huge_library,
-                      style: AppStyles.reg16Yellow.copyWith(
-                        color: AppColors.grayLightColor,
+              child: SafeArea(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        lang.find_next_favorite_movie,
+                        style: AppStyles.bold24White,
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: screenHeight * 0.04),
-                    CustomElevatedButton(
-                      text: lang.explore_now,
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const OnboardingTwo()),
-                        );
-                      },
-                      backgroundColor: AppColors.secondColor,
-                      textStyle: AppStyles.semiBold20Black,
-                    ),
-                  ],
+                      SizedBox(height: screenHeight * 0.015),
+                      Text(
+                        lang.access_huge_library,
+                        style: AppStyles.reg16Yellow.copyWith(
+                          color: AppColors.grayLightColor,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: screenHeight * 0.04),
+                      CustomElevatedButton(
+                        text: lang.explore_now,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const OnboardingTwo()),
+                          );
+                        },
+                        backgroundColor: AppColors.secondColor,
+                        textStyle: AppStyles.semiBold20Black,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
