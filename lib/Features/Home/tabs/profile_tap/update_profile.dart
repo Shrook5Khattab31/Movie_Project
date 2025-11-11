c28b169f35095a961ba2ec7a281da171c89a7a1c
Mariam Osama <mrmrosos86@gmail.com>
2025-11-11 21:42:25 +0200

    + update profile logic

--- lib/Features/Home/tabs/profile_tap/update_profile.dart
+++ lib/Features/Home/tabs/profile_tap/update_profile.dart
@@ -3,138 +3,212 @@ import 'package:movie_project/core/constants/appAssets.dart';
 import 'package:movie_project/core/routing/routeNames.dart';
 import 'package:movie_project/core/widgets/custom_elevated_btn.dart';
 import 'package:movie_project/core/widgets/custom_text_button.dart';
+import 'package:movie_project/core/widgets/custom_text_form_field.dart';
 import '../../../../core/theme/appColors.dart';
 import '../../../../core/theme/appStyles.dart';
+import '../../../../core/utils/custom_dialog.dart';
 import '../../../../l10n/app_localizations.dart';
+import 'package:dio/dio.dart';
 
-class UpdateProfile extends StatelessWidget {
-  UpdateProfile({super.key});
+import '../../../auth/register_screen.dart';
+
+class UpdateProfile extends StatefulWidget {
+  final String loginToken;
+  const UpdateProfile({super.key,required this.loginToken});
+
+  @override
+  State<UpdateProfile> createState() => _UpdateProfileState();
+}
+
+class _UpdateProfileState extends State<UpdateProfile> {
   final List<String> avatars = [
-    AppImages.avatar1,AppImages.avatar2,AppImages.avatar3,
-    AppImages.avatar4,AppImages.avatar5,AppImages.avatar6,
-    AppImages.avatar7,AppImages.avatar8,AppImages.avatar9
+    AppImages.avatar1, AppImages.avatar2, AppImages.avatar3,
+    AppImages.avatar4, AppImages.avatar5, AppImages.avatar6,
+    AppImages.avatar7, AppImages.avatar8, AppImages.avatar9
   ];
+
+  String selectedAvatar = AppImages.avatar1;
+  final TextEditingController emailController = TextEditingController(text: "mai@gmail.com");
+  final TextEditingController phoneController = TextEditingController(text: "01274929396");
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
       appBar: AppBar(
         backgroundColor: AppColors.primaryColor,
-        title:Text(AppLocalizations.of(context)!.pick_avatar,
-          style: AppStyles.reg16Yellow,),
+        title: Text(
+          AppLocalizations.of(context)!.pick_avatar,
+          style: AppStyles.reg16Yellow,
+        ),
         centerTitle: true,
         leading: InkWell(
           onTap: () => Navigator.pop(context),
-          child: Icon(Icons.arrow_back_sharp,
-            color: AppColors.secondColor,),
+          child: Icon(Icons.arrow_back_sharp, color: AppColors.secondColor),
         ),
       ),
       body: Padding(
-        padding: EdgeInsetsGeometry.symmetric(horizontal: width*0.04),
-        child: Column(
-          spacing: height*0.02,
-          children: [
-            GestureDetector(
-              onTap: ()=> showAvatars(context),
-              child: Padding(
-                padding: EdgeInsetsGeometry.directional(top: height*0.03,bottom: height*0.01),
-                child: Image.asset(AppImages.avatar1),
+        padding: EdgeInsets.symmetric(horizontal: width * 0.04),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              SizedBox(height: height * 0.03),
+              GestureDetector(
+                onTap: () => showAvatars(context),
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: height * 0.01),
+                  child: Image.asset(selectedAvatar, height: height * 0.12),
+                ),
               ),
-            ),
-            personInfoBox(text: 'Route Academy', icon: Icons.person_2,
-                height: height*0.06,iconPadding: width*0.04),
-            personInfoBox(text: '01200000000', icon: Icons.phone_rounded,
-                height: height*0.06,iconPadding: width*0.04),
-            Align(
-              alignment: AlignmentGeometry.centerLeft,
-              child: CustomTextButton(
-                text: AppLocalizations.of(context)!.reset_password,
-                onPressed: ()=> Navigator.pushNamed(context, AppRoutes.resetPassScreen),
-                styleText: AppStyles.reg20White
+              CustomTextFormField(
+                controller: emailController,
+                prefixIcon: Icon(Icons.person),
+                hintText: AppLocalizations.of(context)!.email,
+                hintStyle: AppStyles.reg16White,
+                validatorFunc: (text) {
+                  if (text == null || text.trim().isEmpty) {
+                    return "Please enter an Email";
+                  }
+                  return null;
+                },
               ),
-            ),
-            Expanded(child: SizedBox()),
-            CustomElevatedButton(
-              text: AppLocalizations.of(context)!.delete_account,
-              textStyle: AppStyles.reg20White,
-              onPressed: (){},
-              backgroundColor: AppColors.redColor
-            ),
-            CustomElevatedButton(
+              SizedBox(height: height * 0.015),
+              CustomTextFormField(
+                controller: phoneController,
+                hintText: AppLocalizations.of(context)!.phone_number,
+                hintStyle: AppStyles.reg16White,
+                fillColor: AppColors.grayDarkColor,
+                prefixIcon: Icon(Icons.phone),
+                prefixIconColor: AppColors.whiteColor,
+                keyboardType: TextInputType.phone,
+              ),
+              SizedBox(height: height * 0.015),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: CustomTextButton(
+                  text: AppLocalizations.of(context)!.reset_password,
+                  onPressed: () => Navigator.pushNamed(context, AppRoutes.resetPassScreen),
+                  styleText: AppStyles.reg20White,
+                ),
+              ),
+              Expanded(child: SizedBox()),
+              CustomElevatedButton(
+                text: AppLocalizations.of(context)!.delete_account,
+                textStyle: AppStyles.reg20White,
+                onPressed: () {
+                  // هنا تضيفي API call للحذف مع widget.loginToken
+                },
+                backgroundColor: AppColors.redColor,
+              ),
+              SizedBox(height: height * 0.01),
+              CustomElevatedButton(
                 text: AppLocalizations.of(context)!.update_data,
                 textStyle: AppStyles.reg20Black,
-                onPressed: (){},
-                backgroundColor: AppColors.secondColor
-            ),
-            SizedBox(height: height*0.03,)
-          ],
-        ),
-      ),
-    );
-  }
-  Container personInfoBox({required String text, required IconData icon, double height=0,double iconPadding = 16}){
-    return Container(
-      height: height,
-      decoration: BoxDecoration(
-        color: AppColors.grayDarkColor,
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Row(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal:iconPadding),
-            child: Icon(icon),
+                backgroundColor: AppColors.secondColor,
+                onPressed: () async {
+                  if (formKey.currentState!.validate()) {
+                    CustomDialog.showMessage(
+                      context: context,
+                      background: AppColors.primaryColor,
+                      styleMessage: AppStyles.bold14Yellow,
+                      message: 'Waiting...',
+                    );
+
+                    try {
+                      Response response = await apiService.updateProfile(
+                        email: emailController.text,
+                        avatarId: avatars.indexOf(selectedAvatar) + 1,
+                        token: widget.loginToken,
+                      );
+                      print('token:${widget.loginToken}');
+                      CustomDialog.hideLoading(context: context);
+
+                      if (response.data['message'] == "Profile updated successfully") {
+                        CustomDialog.showMessage(
+                          context: context,
+                          background: AppColors.primaryColor,
+                          styleMessage: AppStyles.bold20Yellow,
+                          message: 'Profile updated successfully',
+                        );
+                      } else {
+                        CustomDialog.showMessage(
+                          context: context,
+                          background: AppColors.primaryColor,
+                          styleMessage: AppStyles.bold14Yellow,
+                          message: response.data['message'].toString(),
+                          title: "Error",
+                          posActionName: "ok",
+                        );
+                      }
+                    } catch (e) {
+                      // 5. Exception
+                      CustomDialog.hideLoading(context: context);
+                      CustomDialog.showMessage(
+                        context: context,
+                        background: AppColors.primaryColor,
+                        styleMessage: AppStyles.bold14Yellow,
+                        message: 'Something went wrong',
+                      );
+                    }
+                  }
+                },
+              ),
+              SizedBox(height: height * 0.03),
+            ],
           ),
-          Text(text, style: AppStyles.reg20White,),
-        ],
+        ),
       ),
     );
   }
 
-  Future showAvatars(BuildContext context) async {
+  Future<void> showAvatars(BuildContext context) async {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: EdgeInsetsGeometry.symmetric(horizontal: width*0.03),
-      child: await showModalBottomSheet(
-        context: context,
-        backgroundColor: AppColors.transparentColor,
-        builder: (context) {
-          return Container(
-            height: height*0.46,
-            padding: EdgeInsets.symmetric(vertical: height*0.02,horizontal: width*0.04),
-            decoration: BoxDecoration(
-              color: AppColors.grayDarkColor,
-              borderRadius: BorderRadius.circular(24),
+
+    await showModalBottomSheet(
+      context: context,
+      backgroundColor: AppColors.transparentColor,
+      builder: (context) {
+        return Container(
+          height: height * 0.46,
+          padding: EdgeInsets.symmetric(vertical: height * 0.02, horizontal: width * 0.04),
+          decoration: BoxDecoration(
+            color: AppColors.grayDarkColor,
+            borderRadius: BorderRadius.circular(24),
+          ),
+          child: GridView.builder(
+            itemCount: avatars.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: width * 0.02,
+              mainAxisSpacing: height * 0.02,
             ),
-            margin: EdgeInsets.symmetric(vertical: height*0.02,horizontal: width*0.04),
-            child: GridView.builder(
-              itemCount: avatars.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: width*0.02,
-                mainAxisSpacing: height*0.02,
-              ),
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: height*0.01,horizontal: width*0.02),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: AppColors.secondColor,),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Image.asset(avatars[index], fit: BoxFit.cover),
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedAvatar = avatars[index];
+                  });
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: height * 0.01, horizontal: width * 0.02),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: AppColors.secondColor),
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                );
-              },
-            ),
-          );
-        },
-      ),
+                  child: Image.asset(avatars[index], fit: BoxFit.cover),
+                ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
