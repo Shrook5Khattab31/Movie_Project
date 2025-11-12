4ec3e8792a809ec3394d60b7e0826eee29f310a7
nada <nadamoafia66@gmail.com>
2025-11-12 19:19:54 +0200

    fix:  fix avatarId callback and Add more images to avatar list

--- lib/Features/auth/widget/built_avatar_register.dart
+++ lib/Features/auth/widget/built_avatar_register.dart
@@ -2,26 +2,49 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_project/core/constants/appAssets.dart';
 
-class BuiltAvatarRegister extends StatelessWidget {
-  const BuiltAvatarRegister({super.key});
-  final List avatarList =const [
+class BuiltAvatarRegister extends StatefulWidget {
+  final Function(int)? onAvatarChanged;
+  const BuiltAvatarRegister({super.key, this.onAvatarChanged});
+
+  @override
+  State<BuiltAvatarRegister> createState() => _BuiltAvatarRegisterState();
+}
+
+class _BuiltAvatarRegisterState extends State<BuiltAvatarRegister> {
+  final List avatarList = const [
     AppImages.avatar2,
     AppImages.avatar1,
     AppImages.avatar3,
+    AppImages.avatar4,
+    AppImages.avatar5,
+    AppImages.avatar6,
+    AppImages.avatar7,
+    AppImages.avatar8,
+    AppImages.avatar9
   ];
+  int currentIndex = 1;
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     return CarouselSlider(
       options: CarouselOptions(
-         enlargeCenterPage: true,
-         aspectRatio: 15/6,
-         viewportFraction: height*0.00054,
-         height: height * 0.18,
-         enableInfiniteScroll: false,
-         autoPlay: true,
+        enlargeCenterPage: true,
+        aspectRatio: 15 / 6,
+        viewportFraction: height * 0.00054,
+        height: height * 0.18,
+        enableInfiniteScroll: false,
+        autoPlay: true,
+        initialPage: currentIndex,
+        onPageChanged: (index, reason) {
+          setState(() {
+            currentIndex = index;
+          });
+          if (widget.onAvatarChanged != null) {
+            widget.onAvatarChanged!(index);
+          }
+        },
       ),
-      items: avatarList.map((e){
+      items: avatarList.map((e) {
         return Image.asset(e,fit: BoxFit.cover,);
       },).toList()
     );
