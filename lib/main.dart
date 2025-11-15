b1955903d7914bb6fbec013b4f0572fa500928df
MaiOthman <mai.m.othman94@gmail.com>
2025-11-15 09:12:02 +0100

    I solved the problem of reset password

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_project/Features/Home/tabs/profile_tap/update_profile.dart';
-import 'package:movie_project/Features/auth/forgetPassword.dart';
+import 'package:movie_project/Features/auth/forget_pass.dart';
 import 'Features/Home/home_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:movie_project/Features/Home/tabs/profile_tap/resetPassword.dart';
