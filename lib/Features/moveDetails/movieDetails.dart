5d4ede9600fc5ab9a48c9fb5dc8f198fc69aefce
shrook <shrookabdelmonem55@gmail.com>
2025-11-21 15:48:39 +0200

    +[fix]: localization fixed

--- lib/Features/moveDetails/movieDetails.dart
+++ lib/Features/moveDetails/movieDetails.dart
@@ -25,8 +25,8 @@ class _MovieDetailsState extends State<MovieDetails> {
   }
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height;
-    var width = MediaQuery.of(context).size.width;
+    var height= MediaQuery.of(context).size.height;
+    var width= MediaQuery.of(context).size.width;
     var args = ModalRoute.of(context)!.settings.arguments as Movies;
     return Scaffold(
       body: FutureBuilder<Movie>(
@@ -51,48 +51,48 @@ class _MovieDetailsState extends State<MovieDetails> {
                             alignment: Alignment.topCenter,
                             decoration: BoxDecoration(
                         image: DecorationImage(image: NetworkImage(args.mediumCoverImage??""
-                              ), fit: BoxFit.cover),
-                            ),
-                          ),
-                          Container(
-                            height: height * 0.7,
+                        ),fit: BoxFit.cover),
+                    ),
+                  ),
+                  Container(
+                    height: height*0.7,
                     decoration: BoxDecoration(gradient: LinearGradient(colors: [
-                                AppColors.primaryColorWithObesity80,
-                                AppColors.primaryColorWithObesity60,
-                                AppColors.primaryColor,
-                              ],
-                              begin: FractionalOffset.topCenter,
-                              end: FractionalOffset.bottomCenter,
-                              stops: [
-                                0.0,
-                                0.47,
-                                1.0,
-                              ],
-                            )),
-                          ),
-                          Container(
-                            height: height * 0.7,
+                      AppColors.primaryColorWithObesity80,
+                      AppColors.primaryColorWithObesity60,
+                      AppColors.primaryColor,
+                    ],
+                      begin: FractionalOffset.topCenter,
+                      end: FractionalOffset.bottomCenter,
+                      stops: [
+                        0.0,
+                       0.47,
+                       1.0,
+                      ],
+                    )),
+                  ),
+                 Container(
+                   height: height * 0.7,
                    padding: EdgeInsets.symmetric(horizontal: width*0.03, vertical: height*0.02),
-                            child: Column(
-                              children: [
-                                Row(
+                   child: Column(
+                     children: [
+                       Row(
                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    InkWell(
-                                        onTap: () {
-                                          Navigator.of(context).pop();
-                                        },
+                         children: [
+                           InkWell(
+                               onTap: (){
+                                 Navigator.of(context).pop();
+                               },
                                child: Icon(Icons.arrow_back_ios_outlined, color: AppColors.whiteColor)),
-                                    Icon(Icons.bookmark)
-                                  ],),
-                                Expanded(child: Image.asset(AppImages.play)),
+                           Icon(Icons.bookmark)
+                         ],),
+                       Expanded(child: Image.asset(AppImages.play)),
                        Text(args.title??"", style: AppStyles.bold24White,
-                                  textAlign: TextAlign.center,),
+                         textAlign: TextAlign.center,),
                        Text("${args.year}", style: AppStyles.bold20Gray, textAlign: TextAlign.center,)],),
-                          ),
-                        ],
-                      ),
-                      Container(
+                 ),
+                ],
+              ),
+              Container(
                 padding: EdgeInsets.symmetric(horizontal: width*0.03, vertical: height*0.01),
                           decoration: BoxDecoration(color: Colors.black),
                           child: Column(
@@ -101,11 +101,11 @@ class _MovieDetailsState extends State<MovieDetails> {
                               CustomElevatedButton(
                       onPressed: () {}, backgroundColor: AppColors.redColor,
                       text: AppLocalizations.of(context)!.watch, textStyle: AppStyles.bold20White,
-                                height: 70,
-                              ),
-                              Row(
+                      height: 70,
+                    ),
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
+                      children: [
                         CustomContainer(icon: AppImages.likeIcon, text: "15"),
                         CustomContainer(icon: AppImages.timeIcon, text: "${args.runtime}"),
                         CustomContainer(icon: AppImages.starIcon, text: "${args.rating}")
