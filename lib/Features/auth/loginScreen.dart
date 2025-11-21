1f087d4139baad8a2f32f33cbd670dd78e1b35ad
Aya Emad <ayaelbaroudy0@gmail.com>
2025-11-21 15:33:44 +0200

    + handle avatar to stop(AutoPlay) after click buton

--- lib/Features/auth/loginScreen.dart
+++ lib/Features/auth/loginScreen.dart
@@ -25,8 +25,8 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   var formKey = GlobalKey<FormState>();
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passController = TextEditingController();
+  TextEditingController emailController = TextEditingController(text: "aya@gmail.com");
+  TextEditingController passController = TextEditingController(text: "Aya12345@");
   bool isVisible = false;
 
   @override
