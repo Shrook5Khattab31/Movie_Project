8d558c1971a779edf89bab5fbbe1687310221bb0
Mariam Osama <mrmrosos86@gmail.com>
2025-11-25 19:42:05 +0200

    + browse tab ui and logic

--- lib/Features/Home/tabs/browse_tap/browse_tab.dart
+++ lib/Features/Home/tabs/browse_tap/browse_tab.dart
@@ -1,12 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:movie_project/Model/MoviesModel/Movies.dart';
+import 'package:movie_project/Model/MoviesModel/MovieResponse.dart';
+import 'package:movie_project/api/api_service.dart';
+import 'package:movie_project/core/theme/appColors.dart';
+import 'package:movie_project/core/theme/appStyles.dart';
+import 'package:movie_project/core/widgets/custom_movie_poster.dart';
+import 'package:movie_project/core/routing/routeNames.dart';
 
-class BrowseTabScreen extends StatelessWidget{
+class BrowseTabScreen extends StatefulWidget {
   const BrowseTabScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Placeholder();
+  State<BrowseTabScreen> createState() => _BrowseTabScreenState();
+}
+
+class _BrowseTabScreenState extends State<BrowseTabScreen> {
+  late Future<MovieResponse> moviesFuture;
+  List<Movies> moviesList = [];
+  List<String> genresList = [];
+  String? selectedGenre;
+
+  @override
+  void initState() {
+    super.initState();
+    moviesFuture = ApiService.getAllMovies(page: 1, limit: 50);
   }
 
+  List<Movies> get filteredMovies {
+    if (selectedGenre == null) return [];
+    return moviesList.where((m) => m.genres?.contains(selectedGenre) ?? false).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
+
+    return SafeArea(
+      child: FutureBuilder<MovieResponse>(
+        future: moviesFuture,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          moviesList = snapshot.data!.data?.movies ?? [];
+
+          Set<String> genresSet = {};
+          for (var movie in moviesList) {
+            if (movie.genres != null) genresSet.addAll(movie.genres!);
+          }
+          genresList = genresSet.toList()..sort();
+
+
+          selectedGenre ??= genresList.isNotEmpty ? genresList.first : null;
+
+          return Column(
+            children: [
+              SizedBox(
+                height: height*0.055,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  padding:  EdgeInsets.symmetric(horizontal: width*0.02),
+                  itemCount: genresList.length,
+                  separatorBuilder: (_, __) =>  SizedBox(width: width*0.02),
+                  itemBuilder: (context, index) {
+                    final genre = genresList[index];
+                    final isSelected = genre == selectedGenre;
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedGenre = genre;
+                        });
+                      },
+                      child: Container(
+                        padding:  EdgeInsets.symmetric(horizontal: width*0.04, vertical: height*0.01),
+                        decoration: BoxDecoration(
+                          color: isSelected ? AppColors.secondColor : AppColors.transparentColor,
+                          borderRadius: BorderRadius.circular(16),
+                          border: Border.all(color: AppColors.secondColor, width: width*0.005),
+                        ),
+                        child: Center(
+                          child: Text(
+                            genre,
+                            style: isSelected ? AppStyles.bold20Black : AppStyles.bold20Yellow
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height:height*0.02 ),
+              Expanded(
+                child: GridView.builder(
+                  padding:  EdgeInsets.symmetric(horizontal: width*0.02,vertical: height*0.02),
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: filteredMovies.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 3/4,
+                    mainAxisSpacing: height*0.016,
+                    crossAxisSpacing: width*0.02,
+
+                  ),
+                  itemBuilder: (context, index) {
+                    final movie = filteredMovies[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pushNamed(
+                          AppRoutes.detailsScreen,
+                          arguments: movie,
+                        );
+                      },
+                      child: CustomMoviePoster(
+                        imageWidth: width*0.5,
+                        imageHeight: height*0.3,
+                        image: movie.mediumCoverImage ?? '',
+                        rating: movie.rating,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
 }
